f68cf709deec499f54d72789c38093187e4435b9
BimaPn <bimaptr12@gmail.com>
2022-02-05 12:05:11 +0700

    menambahkan file js

---
+++ index.js
@@ -0,0 +1,9 @@
+document.getElementById("myProjects").onclick = function(){
+    alert("project ini belum selesai")
+}
+document.getElementById("myProjects2").onclick = function(){
+    alert("project ini belum selesai")
+}
+document.getElementById("myProjects3").onclick = function(){
+    alert("project ini belum selesai")
+}
